41db412794fbbe92b3802a9fb7817500c635edfc
Leo Lee <tl.leolee@gmail.com>
2020-08-12 17:49:52 -0700

    clear MONGODB_URI

--- server/src/data-access/test.js
+++ server/src/data-access/test.js
@@ -5,9 +5,8 @@ const schemas = require("./mongo/schema");
 const { UserSchema, IssueSchema, ProjectSchema } = schemas;
 const { mongoDatabase } = require("./mongo/database");
 
-const MONGODB_URI = "mongodb+srv://admin:123@cluster0-yp8zg.mongodb.net/issue-tracker?retryWrites=true&w=majority";
 function connectionFactory() {
-  const conn = mongoose.createConnection(MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true });
+  const conn = mongoose.createConnection(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true });
   conn.model('User', UserSchema);
   conn.model("Issue", IssueSchema);
   conn.model("Project", ProjectSchema);
